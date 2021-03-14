7b85bbf71a6a9c66c552291b4c12c19cfd26af28
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-03-15 02:54:37 +0700

    lua

--- lua/tree-sitter/init.lua
+++ lua/tree-sitter/init.lua
@@ -1,5 +1,3 @@
-" configure treesitter
-lua << EOF
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
@@ -12,4 +10,3 @@ require'nvim-treesitter.configs'.setup {
     enable = false
   }
 }
-EOF
