49c67670821a05025ce53e45aacb9fcf2b6de2a2
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-03-17 14:47:33 +0700

    edit install

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ if [-d "$HOME/.config/nvim"];
 then
     mv $HOME/.config/nvim $HOME/.config/nvim.old
     git clone https://github.com/tranvietphuoc/nvim.git $HOME/.config/
+    cd $HOME/.config/nvim/
+    rm -r ./assets/
 else
     git clone https://github.com/tranvietphuoc/nvim.git $HOME/.config/
+    cd $HOME/.config/nvim/
+    rm -r ./assets/
 if
