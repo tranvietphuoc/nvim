01a940a701dbbeb9f85f091fdff1d4de61b1d6cc
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-03-26 17:15:01 +0700

    fix some command

--- lua/providers.lua
+++ lua/providers.lua
@@ -1,6 +1,4 @@
-vim.cmd([[
-    if finddir('.venv', '.;')
-        let g:python3_host_prog = './.venv/bin/python'
-    endif
-    let g:python3_host_prog = '/usr/local/bin/python'
-]])
+if vim.fn.finddir('.venv', '.;') then
+    vim.g.python3_host_prog = "./venv/bin/python"
+end
+vim.g.python3_host_prog = "/usr/local/bin/python"
--- lua/vcsv/init.lua
+++ lua/vcsv/init.lua
@@ -1,3 +1,3 @@
-vim.cmd('let b:csv_arrange_align = \'l*\'')
-vim.cmd('let b:csv_no_progress = 1')
-vim.cmd('let b:csv_arrange_use_all_rows = 1')
+vim.g.csv_arrange_align = 'l*'
+vim.g.csv_no_progress = 1
+vim.csv_arrange_use_all_rows = 1
