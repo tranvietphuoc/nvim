6f0bbb46db8b29d68d433b80c61ca89b9bdcda0e
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-04-01 04:13:59 +0700

    edit config

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,4 +1,4 @@
 -- vim.cmd('set laststatus=2')
 vim.cmd('set background=dark')
-vim.cmd('colorscheme PaperColor')
+vim.cmd('colorscheme ' .. O.colorscheme)
 vim.api.nvim_command('let g:PaperColor_Theme_Options = { \'language\': { \'python\': { \'highlight_builtins\': 1}, \'cpp\': { \'highlight_standard_library\': 1 }, \'c\': { \'highlight_builtins\': 1 } } }')
