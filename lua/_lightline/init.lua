a3318b80501a31f3f752e7d9818133cb307b66c3
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-04-04 05:56:25 +0700

    edit lightline

--- lua/_lightline/init.lua
+++ lua/_lightline/init.lua
@@ -25,26 +25,17 @@ vim.cmd('let g:lightline#bufferline#shorten_path = 1')
 vim.cmd('let g:lightline#bufferline#unnamed = \'[No Name]\'')
 vim.cmd('let g:lightline.component_raw = {\'buffers\': 1}')
 
-vim.cmd[[ nmap <Leader>1 :call lightline#bufferline#go(1)<CR> ]]
-vim.cmd[[ nmap <Leader>2 :call lightline#bufferline#go(2)<CR> ]]
-vim.cmd[[ nmap <Leader>3 :call lightline#bufferline#go(3)<CR> ]]
-vim.cmd[[ nmap <Leader>4 :call lightline#bufferline#go(4)<CR> ]]
-vim.cmd[[ nmap <Leader>5 :call lightline#bufferline#go(5)<CR> ]]
-vim.cmd[[ nmap <Leader>6 :call lightline#bufferline#go(6)<CR> ]]
-vim.cmd[[ nmap <Leader>7 :call lightline#bufferline#go(7)<CR> ]]
-vim.cmd[[ nmap <Leader>8 :call lightline#bufferline#go(8)<CR> ]]
-vim.cmd[[ nmap <Leader>9 :call lightline#bufferline#go(9)<CR> ]]
-vim.cmd[[ nmap <Leader>0 :call lightline#bufferline#go(10)<CR> ]]
--- delete buffer with particular number
-vim.cmd[[ nmap <S-1> :call lightline#bufferline#delete(1)<CR> ]]
-vim.cmd[[ nmap <S-2> :call lightline#bufferline#delete(2)<CR> ]]
-vim.cmd[[ nmap <S-3> :call lightline#bufferline#delete(3)<CR> ]]
-vim.cmd[[ nmap <S-4> :call lightline#bufferline#delete(4)<CR> ]]
-vim.cmd[[ nmap <S-5> :call lightline#bufferline#delete(5)<CR> ]]
-vim.cmd[[ nmap <S-6> :call lightline#bufferline#delete(6)<CR> ]]
-vim.cmd[[ nmap <S-7> :call lightline#bufferline#delete(7)<CR> ]]
-vim.cmd[[ nmap <S-8> :call lightline#bufferline#delete(8)<CR> ]]
-vim.cmd[[ nmap <S-9> :call lightline#bufferline#delete(9)<CR> ]]
-vim.cmd[[ nmap <S-0> :call lightline#bufferline#delete(10)<CR> ]]
 
+-- move to particular buffer
+
+vim.api.nvim_set_keymap('n',  '<Leader>1',  ':call lightline#bufferline#go(1)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>2',  ':call lightline#bufferline#go(2)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>3',  ':call lightline#bufferline#go(3)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>4',  ':call lightline#bufferline#go(4)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>5',  ':call lightline#bufferline#go(5)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>6',  ':call lightline#bufferline#go(6)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>7',  ':call lightline#bufferline#go(7)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>8',  ':call lightline#bufferline#go(8)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>9',  ':call lightline#bufferline#go(9)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n',  '<Leader>0',  ':call lightline#bufferline#go(10)<CR>', { noremap = true, silent = true })
 
