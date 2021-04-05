efe967a056040541efe382175270333f0d666556
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-04-06 01:27:10 +0700

    edit tsserver

--- lua/lsp/tsserver-lsp.lua
+++ lua/lsp/tsserver-lsp.lua
@@ -5,14 +5,13 @@ require'lspconfig'.tsserver.setup {
     on_attach = require'lsp'.tsserver_on_attach,
     -- on_attach = require'lsp'.common_on_attach,
     root_dir = require('lspconfig/util').root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
-    settings = {documentFormatting = false},
+    settings = {documentFormatting = true},
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
             virtual_text = O.tsserver.diagnostics.virtual_text,
             signs = O.tsserver.diagnostics.signs,
             underline = O.tsserver.diagnostics.underline,
-            update_in_insert = true
-
+            update_in_insert = true,
         })
     }
 }
