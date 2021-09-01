d9c35fc481e5879ca4cb37e7c13a7df9f3d92e89
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-09-01 18:38:08 +0700

    rm lazygit

--- lua/n-lazygit/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.lazygit_floating_window_winblend = 0
-vim.g.lazygit_floating_window_scaling_factor = 0.9  -- scaling factor for floating window
-vim.g.lazygit_floating_window_corner_chars = {'╭', '╮', '╰', '╯'} --customize lazygit popup window corner characters
-vim.g.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
-vim.g.lazygit_use_neovim_remote = 0
-
-vim.api.nvim_set_keymap('n', '<Leader>L', ':LazyGit<CR>', { noremap = true, silent = true })
