6b347656e7a3ee092b41532a3029b48422e2985c
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-10-05 04:11:17 +0700

    update keymapping

--- lua/extensions/barbar.lua
+++ lua/extensions/barbar.lua
@@ -4,13 +4,13 @@ vim.api.nvim_set_keymap('n', '<Space><Left>', ':BufferPrevious<CR>', { noremap =
 vim.api.nvim_set_keymap('n', '<Space>c', ':BufferClose<CR>', { noremap = true, silent = true })
 -- Re-order to previous/next
 -- Goto buffer in position...
-vim.cmd[[ nnoremap <silent>    1 :BufferGoto 1<CR> ]]
-vim.cmd[[ nnoremap <silent>    2 :BufferGoto 2<CR> ]]
-vim.cmd[[ nnoremap <silent>    3 :BufferGoto 3<CR> ]]
-vim.cmd[[ nnoremap <silent>    4 :BufferGoto 4<CR> ]]
-vim.cmd[[ nnoremap <silent>    5 :BufferGoto 5<CR> ]]
-vim.cmd[[ nnoremap <silent>    6 :BufferGoto 6<CR> ]]
-vim.cmd[[ nnoremap <silent>    7 :BufferGoto 7<CR> ]]
-vim.cmd[[ nnoremap <silent>    8 :BufferGoto 8<CR> ]]
-vim.cmd[[ nnoremap <silent>    9 :BufferGoto 9<CR> ]]
-vim.cmd[[ noremap <silent>     0 :BufferLast<CR>]]
+vim.cmd[[ nnoremap <silent>    <Space>1 :BufferGoto 1<CR> ]]
+vim.cmd[[ nnoremap <silent>    <Space>2 :BufferGoto 2<CR> ]]
+vim.cmd[[ nnoremap <silent>    <Space>3 :BufferGoto 3<CR> ]]
+vim.cmd[[ nnoremap <silent>    <Space>4 :BufferGoto 4<CR> ]]
+vim.cmd[[ nnoremap <silent>    <Space>5 :BufferGoto 5<CR> ]]
+vim.cmd[[ nnoremap <silent>    <Space>6 :BufferGoto 6<CR> ]]
+vim.cmd[[ nnoremap <silent>    <Space>7 :BufferGoto 7<CR> ]]
+vim.cmd[[ nnoremap <silent>    <Space>8 :BufferGoto 8<CR> ]]
+vim.cmd[[ nnoremap <silent>    <Space>9 :BufferGoto 9<CR> ]]
+vim.cmd[[ noremap <silent>     <Space>0 :BufferLast<CR>]]
