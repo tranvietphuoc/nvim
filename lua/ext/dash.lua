67f5dfa2d1daa7d5894020cb35351221a52aebaf
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-10-29 05:23:23 +0700

    edit dashboard

--- lua/ext/dash.lua
+++ lua/ext/dash.lua
@@ -2,14 +2,12 @@ local M = {}
 
 function M.setup()
     vim.g.dashboard_custom_header = {
-        "                                                  ",
         "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
         "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
         "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
         "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
         "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
         "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
-        "                                                  ",
     }
 
     vim.g.dashboard_default_executive = "Telescope"
@@ -22,10 +20,11 @@ function M.setup()
         e = { description = { "  Settings            " }, command = ":e ~/.config/nvim/lua/config/init.lua" },
     }
     vim.g.indentLine_fileTypeExclude = { "dashboard" }
+    vim.g.indentLine_fileTypeExclude = { "dashboard" }
     vim.g.dashboard_custom_footer = { "Configured by Tran Viet Phuoc" }
-    vim.cmd("autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2")
+    -- vim.cmd("autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2")
     vim.g.dashboard_preview_file_height = 12
-    vim.g.dashboard_preview_file_width = 80
+    vim.g.dashboard_preview_file_width = 100
 end
 
 return M
