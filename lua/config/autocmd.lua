fc86e15d3cf173b4b1cf0727a4453b90f0281b66
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2022-09-20 00:27:09 +0700

    remove EmmetInstall autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -6,8 +6,8 @@ function M.auto_cmds()
     vim.g.dracula_italic_comment = true
     vim.g.dracula_transparent_bg = false
 
-    vim.g.bracey_refresh_on_save = 1
-    vim.g.bracey_bracey_eval_on_save = 1
+    -- vim.g.bracey_refresh_on_save = 1
+    -- vim.g.bracey_bracey_eval_on_save = 1
     cmd("autocmd BufRead,BufNewFile *.js,*.ts,*.tsx,*.jsx,*.json setlocal expandtab ts=2 sw=2 sts=2")
     cmd("autocmd BufRead,BufNewFile *.html,*.css,*.scss,*.yml,*.svelte setlocal expandtab ts=2 sw=2 sts=2")
     cmd("autocmd BufNewFile,BufRead *.go setlocal noexpandtab ts=8 sw=8")
@@ -27,7 +27,7 @@ function M.auto_cmds()
 
     cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
     -- cmd("autocmd User TelescopePreviewerLoaded setlocal wrap")
-    cmd("autocmd FileType html,css,htmldjango,sass EmmetInstall")
+    -- cmd("autocmd FileType html,css,htmldjango,sass EmmetInstall")
 end
 
 function M.setup()
