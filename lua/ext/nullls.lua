048f2d2cd3e32d76b64bbc55361df669b040949b
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2022-09-27 16:56:52 +0700

    edit null-ls

--- lua/ext/nullls.lua
+++ lua/ext/nullls.lua
@@ -1,88 +1,16 @@
 local M = {}
 
--- define formatters
-
-
 function M.setup()
-    -- local black = function()
-    --     return { exe = "black", args = { "-l 80", "-q", "-" }, stdin = true }
-    -- end
-    --
-    -- local prettier = function()
-    --     return {
-    --         exe = "prettier",
-    --         args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote" },
-    --         stdin = true,
-    --     }
-    -- end
-    --
-    -- local clangfmt = function()
-    --     return {
-    --         exe = "clang-format",
-    --         args = { "-style=file", "--assume-filename", vim.api.nvim_buf_get_name(0) },
-    --         stdin = true,
-    --         cwd = vim.fn.expand("%:p:h"), -- run clang-format in cwd of the file
-    --     }
-    -- end
-    --
-    -- local rustfmt = function()
-    --     return { exe = "rustfmt", args = { "--emit=stdout" }, stdin = true }
-    -- end
-    --
-    -- -- local luafmt = function ()
-    -- --   return { exe = "luafmt", args = {"--indent-count", 2, "--stdin"}, stdin = true }
-    -- -- end
-    --
-    -- local gofmt = function()
-    --     return {
-    --         exe = "gofmt",
-    --         args = { vim.api.nvim_buf_get_name(0) },
-    --         stdin = true,
-    --     }
-    -- end
-    --
-    -- local stylua = function()
-    --     return {
-    --         exe = "stylua",
-    --         args = {
-    --             "--config-path " .. ".stylua.toml",
-    --             "-",
-    --         },
-    --         stdin = true,
-    --     }
-    -- end
-    --
-    -- -- config
-    -- require("formatter").setup({
-    --     filetype = {
-    --         python = { black },
-    --         javascript = { prettier },
-    --         typescript = { prettier },
-    --         javascriptreact = { prettier },
-    --         typescriptreact = { prettier },
-    --         html = { prettier },
-    --         htmldjango = { prettier },
-    --         css = { prettier },
-    --         rust = { rustfmt },
-    --         cpp = { clangfmt },
-    --         c = { clangfmt },
-    --         cc = { clangfmt },
-    --         h = { clangfmt },
-    --         go = { gofmt },
-    --         lua = { stylua },
-    --     },
-    -- })
-    --
-    local async_formatting = function (bufnr)
+    local async_formatting = function(bufnr)
         bufnr = bufnr or vim.api.nvim_get_current_buf()
 
         vim.lsp.buf_request(
             bufnr,
             "textDocument/formatting",
             vim.lsp.util.make_formatting_params({}),
-            function (err, res, ctx)
+            function(err, res, ctx)
                 if err then
-                    local err_msg = type(err) == 'string' and err or err.message
+                    local err_msg = type(err) == "string" and err or err.message
                     vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
                     return
                 end
@@ -94,7 +22,7 @@ function M.setup()
                 if res then
                     local client = vim.lsp.get_client_by_id(ctx.client_id)
                     vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-                    vim.api.nvim_buf_call(bufnr, function ()
+                    vim.api.nvim_buf_call(bufnr, function()
                         vim.cmd("silent noautocmd update")
                     end)
                 end
@@ -121,23 +49,21 @@ function M.setup()
     -- completion sources
     -- local completion = null_ls.builtins.completion
 
-
-
     null_ls.setup({
         debug = false,
         sources = {
             formatting.stylua.with({
-                args = { "--config-path", vim.fn.expand("~/.config/nvim/.stylua.toml") }
+                extra_args = { "--config-path", vim.fn.expand("~/.stylua.toml") },
             }),
             diagnostics.eslint,
             code_actions.eslint,
             code_actions.gitsigns,
             -- completion.luasnip,
             formatting.prettier.with({
-                args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote" }
+                args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote" },
             }),
             formatting.rustfmt.with({
-                args = { "--emit=stdout" }
+                args = { "--emit=stdout" },
             }),
             formatting.gofmt,
             formatting.black.with({
