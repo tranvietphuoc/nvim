9f4a25ea23e0903e5cb81d961073f881a6a0dd8b
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2022-09-28 14:44:49 +0700

    edit lspsaga

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,5 +1,5 @@
 local function map(...)
-    vim.api.nvim_set_keymap(...)
+    vim.keymap.set(...)
 end
 
 local function buf_map(...)
@@ -26,60 +26,7 @@ local saga = require("lspsaga")
 
 function M.setup()
     -- lsp saga
-    saga.setup({
-        -- code_action_prompt = {
-        --     enable = true,
-        --     sign = true,
-        --     sign_priority = 20,
-        --     virtual_text = true,
-        -- },
-        debug = false,
-        use_saga_diagnostic_sign = true,
-        -- diagnostic sign
-        error_sign = "",
-        warn_sign = "",
-        hint_sign = "",
-        infor_sign = "",
-        diagnostic_header_icon = "   ",
-        -- code action title icon
-        code_action_icon = " ",
-        code_action_prompt = {
-            enable = true,
-            sign = true,
-            sign_priority = 40,
-            virtual_text = true,
-        },
-        finder_definition_icon = "  ",
-        finder_reference_icon = "  ",
-        max_preview_lines = 10,
-        finder_action_keys = {
-            open = "o",
-            vsplit = "s",
-            split = "i",
-            quit = "q",
-            scroll_down = "<C-f>",
-            scroll_up = "<C-b>",
-        },
-        code_action_keys = {
-            quit = "q",
-            exec = "<CR>",
-        },
-        rename_action_keys = {
-            quit = "<C-c>",
-            exec = "<CR>",
-        },
-        definition_preview_icon = "  ",
-        border_style = "single",
-        rename_prompt_prefix = "➤",
-        rename_output_qflist = {
-            enable = false,
-            auto_open_qflist = false,
-        },
-        server_filetype_map = {},
-        diagnostic_prefix_format = "%d. ",
-        diagnostic_message_format = "%m %c",
-        highlight_prefix = false,
-    })
+    saga.init_lsp_saga({})
 
     -- lsp config
     -- map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
@@ -108,23 +55,37 @@ function M.setup()
 
     -- lspsaga
     -- lsp finder
-    map("n", "gh", "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>", opts)
+    map("n", "gh", "<cmd>Lspsaga lsp_finder<CR><CR>", opts)
+
     -- code actions
-    map("n", "<space>ca", "lua require('lspsaga.codeaction').code_action()<CR>", opts)
+    map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+
     -- signature help
     map("n", "gs", "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>", opts)
+
     -- preview definition
-    map("n", "gd", "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>", opts)
+    map("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
 
     -- rename
-    map("n", "gr", "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
+    map("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
+
     -- diagnostics
-    map("n", "[d", "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>", opts)
-    map("n", "]d", "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>", opts)
+    map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+    map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+
     -- hover
-    map("n", "K", "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
-    map("n", "<C-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
-    map("n", "<C-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
+    map("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+
+    -- Only jump to error
+    map("n", "[E", function()
+        require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+    end, opts)
+    map("n", "]E", function()
+        require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+    end, opts)
+
+    -- Outline
+    map("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
 end
 
 return M
