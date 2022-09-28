f3d490f6990a8a146a76810dc68b1223dee2a253
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2022-09-28 17:54:13 +0700

    edit sqls servers

--- lua/lsp/sql.lua
+++ lua/lsp/sql.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
     require("lspconfig").sqls.setup({
-        cmd = { DATA .. "/lsp_servers/sqls/sqls", "-config", "./config.yml" },
+        cmd = { DATA .. "/lsp_servers/sqls/sqls", "-config", "./sqlrc.yml" },
         filetypes = { "sql" },
         single_file_support = true,
     })
