416f30eb820abd656e38fe2ba19b559163021e63
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2022-11-13 19:24:15 +0700

    rm redundant folers

--- lua/lua/ext/git-blame.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.setup()
-    vim.g.gitblame_date_format = "%r"
-    vim.g.gitblame_message_template = "<summary> • <date> • <author>"
-    vim.g.gitblame_enabled = 1
-end
-
-return M
--- lua/lua/lsp/graphql.lua
+++
@@ -1,6 +0,0 @@
--- npm install -g graphql-language-service-cli
-local M = {}
-function M.setup()
-    require("lspconfig").graphql.setup({ on_attach = require("lsp").common_on_attach })
-end
-return M
