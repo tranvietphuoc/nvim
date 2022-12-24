f6041ae81ef101d2a681d31cd5e39dcb7343a70a
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2022-12-25 04:43:31 +0700

    cleanup files

--- lua/lsp/cpp.lua
+++ lua/lsp/cpp.lua
@@ -14,7 +14,7 @@ function M.setup()
         extensions = {
             -- defaults:
             -- Automatically set inlay hints (type hints)
-            autoSetHints = true,
+            autoSetHints = false,
         },
         ast = {
             -- These are unicode, should be available in any font
@@ -41,42 +41,6 @@ function M.setup()
         },
 
     })
-    --[[ require("lspconfig").clangd.setup({
-        cmd = { DATA .. "/mason/bin/clangd" },
-        on_attach = require("lsp").common_on_attach,
-        -- handlers = {
-        -- ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.diagnostic.on_publish_diagnostics, {
-        -- virtual_text = true,
-        -- signs = true,
-        -- underline = true,
-        -- update_in_insert = true,
-        -- })
-        -- },
-        filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto", "cc", "h", "hpp" },
-        handlers = lsputils.lsp_diagnostics(),
-        settings = {
-            autoSetHints = true,
-        }
-    }) ]]
-    --[[ require("lspconfig").ccls.setup({
-        cmd = { DATA .. "/lsp_servers/ccls/bin/ccls" },
-        filetype = { "c", "cpp", "cc", "h", "hpp" },
-        offset_encoding = "utf-8",
-        root_dir = require("lspconfig/util").root_pattern("compile_commands.json", ".ccls", ".git"),
-        single_file_support = false,
-        cache = {
-            directory = ".ccls-cache",
-        },
-        init_options = {
-            compilationDatabaseDirectory = "build",
-            index = {
-                threads = 0,
-            },
-            clang = {
-                excludeArgs = { "-frounding-math" },
-            },
-        },
-    }) ]]
 end
 
 return M
