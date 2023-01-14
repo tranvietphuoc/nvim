62403fdc2fbec2f645f53185ce9d3bfc58e245e4
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2023-01-15 00:25:34 +0700

    eidt gitsign and git-blame config

--- lua/ext/git-blame.lua
+++ lua/ext/git-blame.lua
@@ -2,8 +2,9 @@ local M = {}
 
 function M.setup()
     vim.g.gitblame_date_format = "%r"
-    vim.g.gitblame_message_template = "<summary> • <date> • <author>"
+    vim.g.gitblame_message_template = "<committer> • <sha> • <summary> • <date> • <author>"
     vim.g.gitblame_enabled = 1
+    vim.g.gitblame_highlight_group = "Question"
 end
 
 return M
