2cdfd4084c88e67e4fac25a42bd0e8045b6e8816
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2023-08-05 01:27:48 +0700

    edit colorscheme

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -13,7 +13,7 @@ function globals.init()
 
     g.mapleader = ","
 
-    cmd("filetype plugin indent on") -- filetype detection
+    cmd("filetype plugin indent on")  -- filetype detection
     cmd("set showcmd")
     cmd("set whichwrap+=<,>,[,],h,l") -- move to next line with theses keys
     cmd("set modifiable")
@@ -23,7 +23,7 @@ function globals.init()
     cmd("set inccommand=split")
     cmd("set shortmess+=c")
     -- cmd([[colorscheme dracula]])
-    cmd([[colorscheme tokyonight-moon]])
+    cmd([[colorscheme tokyonight-night]])
     -- cmd([[colorscheme dawnfox]])
 
     -- opt.shortmess:append("c") -- Don't pass messages to |ins-completion-menu|.
@@ -37,8 +37,8 @@ function globals.init()
     o.titlestring = "%<%F%=%l/%L - nvim"
     -- o.completeopt = "menuone,noselect"
     vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
-    opt.tabstop = 4 -- Insert 2 spaces for a tab
-    opt.shiftwidth = 4 -- Change the number of space characters inserted for indentation
+    opt.tabstop = 4      -- Insert 2 spaces for a tab
+    opt.shiftwidth = 4   -- Change the number of space characters inserted for indentation
     opt.expandtab = true -- Converts tabs to spaces
     opt.softtabstop = 4
     o.breakindent = true
@@ -83,7 +83,7 @@ function globals.init()
     -- clipboard
     opt.clipboard:append("unnamed,unnamedplus")
 
-    opt.iskeyword:append("-") -- treat dash seperate words as word
+    opt.iskeyword:append("-")                     -- treat dash seperate words as word
     opt.runtimepath:append("/usr/local/opt/fzf/") --runtimepath
 
     g.doge_doc_standard_python = true
