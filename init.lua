e65f921bdf4a57129414803d3b88a003f334f773
Tran Viet Phuoc <46410824+tranvietphuoc@users.noreply.github.com>
2023-08-26 17:48:14 +0000

    Update init.lua  -- remove packer

--- init.lua
+++ init.lua
@@ -1,16 +1,3 @@
-local fn = vim.fn
-local execute = vim.api.nvim_command
-
-local function packer_init()
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-    end
-    -- vim.cmd("packadd packer.nvim")
-    vim.cmd("autocmd BufWritePost plugins.lua PackerCompile")
-end
-
-packer_init()
 
 -- general
 require("config.autocmd").setup()
