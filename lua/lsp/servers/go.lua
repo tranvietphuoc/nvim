4f77d45415ea4ae8722d3f2b77e9b6c78f559dbe
phuoc <phuoc@phuocs-MacBook-Air.local>
2023-11-24 09:22:17 +0700

    edit golang

--- lua/lsp/servers/go.lua
+++ lua/lsp/servers/go.lua
@@ -7,29 +7,13 @@ local lsp_flags = {
 
 function M.setup()
     require("lspconfig").gopls.setup({
-        cmd = { DATA .. "/mason/bin/gopls", "-remote.debug=:0" },
+        cmd = { DATA .. "/mason/bin/gopls" },
         flags = lsp_flags,
-        cmd_env = {
-            GLOB_PATTERN = "*@(.sh|.inc|.bash|.command)",
-        },
         filetypes = { "go", "gomod", "gowork", "gotmpl" },
         settings = {
             gopls = {
-                analyses = {
-                    unreachable = true,
-                    nilness = true,
-                    unusedparams = true,
-                    useany = true,
-                    unusedwrite = true,
-                    ST1003 = true,
-                    undeclaredname = true,
-                    fillreturns = true,
-                    nonewvars = true,
-                    fieldalignment = false,
-                    shadow = true,
-                },
+                analyses = { unusedparams = true },
                 usePlaceholders = true,
-                completeUnimported = true,
                 staticcheck = true,
                 gofumpt = true,
                 hints = {
