e3b82560f734df880914cc4967ce30c8b3c33e2b
tranvietphuoc <phuoctv.ut@gmail.com>
2024-10-05 01:37:15 +0700

    update config for arch-wsl

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -45,7 +45,8 @@ function M.setup()
                     ensure_installed = { "stylua", "checkstyle", "clang-format", "prettier",
                         "pg-format",
                         "scalafmt", "pycodestyle", "csharpier", "google-java-format", "mypy", "eslint_d",
-                        "gitsigns", "java-debug-adapter", "cppcheck", "pylint", "markdownlint", "djlint", "scalafmt", "ruff",
+                        "gitsigns", "java-debug-adapter", "cppcheck", "pylint", "markdownlint", "djlint", "scalafmt",
+                        "ruff",
                         "djhtml",
                         "vaccum",
                         "dotenv_linter",
@@ -513,7 +514,7 @@ function M.setup()
             config = function()
                 require('distant'):setup()
             end
-        }
+        },
         --[[ {
             'wfxr/minimap.vim',
             build = "cargo install --locked code-minimap",
@@ -521,6 +522,48 @@ function M.setup()
                 vim.g.minimap_auto_start = true
                 vim.g.minimap_width = 10
             end,
+        } ]]
+
+        -- roslyn
+        --[[ {
+            "seblj/roslyn.nvim",
+            ft = "cs",
+            on_attach = function(client, bufnr)
+                require("lsp").common_on_attach(client, bufnr)
+            end,
+
+            opts = {
+                -- your configuration comes here; leave empty for default settings
+                config = {
+                    settings = {
+                        ["csharp|inlay_hints"] = {
+                            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                            csharp_enable_inlay_hints_for_types = true,
+                            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                            dotnet_enable_inlay_hints_for_literal_parameters = true,
+                            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                            dotnet_enable_inlay_hints_for_other_parameters = true,
+
+                            dotnet_enable_inlay_hints_for_parameters = true,
+
+                            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+
+                        },
+                        ["csharp|code_lens"] = {
+                            dotnet_enable_references_code_lens = true,
+                        },
+                    }
+                },
+                exe = { "dotnet",
+                    vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
+                },
+                filewatching = true,
+                choose_sln = nil,
+            }
         } ]]
     })
 end
