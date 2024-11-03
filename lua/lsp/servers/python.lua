c6260559811f2d389867018f42c046045a7b0276
tranvietphuoc <phuoctv.ut@gmail.com>
2024-11-03 23:27:17 +0700

    pylsp ruff

--- lua/lsp/servers/python.lua
+++ lua/lsp/servers/python.lua
@@ -73,6 +73,29 @@ function M.setup()
                         report_progress = true,
                         live_mode = false,
                     },
+                    ruff = {
+                        enabled = true, -- Enable the plugin
+                        formatEnabled = true, -- Enable formatting using ruffs formatter
+
+                        executable = DATA .. "/mason/bin/ruff", -- Custom path to ruff
+
+                        config = "pyproject.toml", -- Custom config for ruff to use
+                        extendSelect = { "I" }, -- Rules that are additionally used by ruff
+                        extendIgnore = { "C90" }, -- Rules that are additionally ignored by ruff
+                        format = { "I" }, -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
+                        severities = { ["D212"] = "I" }, -- Optional table of rules where a custom severity is desired
+
+                        unsafeFixes = false, -- Whether or not to offer unsafe fixes as code actions. Ignored with the "Fix All" action
+
+                        -- Rules that are ignored when a pyproject.toml or ruff.toml is present:
+                        lineLength = 88, -- Line length to pass to ruff checking and formatting
+                        exclude = { "__about__.py" }, -- Files to be excluded by ruff checking
+                        select = { "F" }, -- Rules to be enabled by ruff
+                        ignore = { "D210" }, -- Rules to be ignored by ruff
+                        perFileIgnores = { ["__init__.py"] = "CPY001" }, -- Rules that should be ignored for specific files
+                        preview = false, -- Whether to enable the preview style linting and formatting.
+                        targetVersion = "py310", -- The minimum python version to target (applies for both linting and formatting).
+                    },
                 },
             },
         },
